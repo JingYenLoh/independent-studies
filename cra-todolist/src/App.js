300834c88a2dc2c95f1608701178dba31eeda990
Jing Yen Loh <lohjingyen@gmail.com>
2018-05-14 12:28:27 +0800

    Connect app to redux store

--- cra-todolist/src/App.js
+++ cra-todolist/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import AddTodoForm from "./containers/AddTodoForm";
+import TodoList from "./containers/TodoList";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const store = createStore(rootReducer);
+
+const App = () => (
+  <Provider store={store}>
+    <Fragment>
+      <AddTodoForm />
+      <TodoList />
+    </Fragment>
+  </Provider>
+);
 
 export default App;
